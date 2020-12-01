5f4cbab0792e921290422478642b15168baf8691
ltquiambao <lorenzotquiambao@gmail.com>
2020-12-01 01:53:31 -0500

    final version for now

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.set('view engine', 'ejs');
 
 // static files
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 // routing
 app.get('/init', (req, res) => {
